f49ab90cb7201ad0eba68342938a3c191dd85b47
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-01-30 21:42:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   else{
+    tots= total()
     setCart([]);
-    return `Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${tots}, which will be charged to the card ${cardNumber}.`;
   }
 }
